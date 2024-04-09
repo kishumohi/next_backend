a5a35cebe22fdbdef70fbda23a7535be27630614
kishankalavadiya@gmail.com <kishankalavadiya@gmail.com>
2024-04-09 17:17:47 +0530

    Final Complete

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,19 +6,19 @@ export function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname;
 
   const isPublicPath =
-    path === "/login" || path === "signup" || path === "/verifyemail";
+    path === "/login" || path === "/signup" || path === "/verifyemail";
 
   const token = request.cookies.get("token")?.value || "";
 
-  // if (isPublicPath && token) {
-  //   return NextResponse.redirect(new URL("/", request.url));
-  // }
+  if (isPublicPath && token) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
   if (!isPublicPath && !token) {
-    return NextResponse.redirect(new URL("/login", request.url));
+    return NextResponse.redirect(new URL("/signup", request.url));
   }
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ["/", "/login", "/signup", "/profile", "/verifyemail"],
+  matcher: ["/", "/signup", "/login", "/profile", "/verifyemail"],
 };
