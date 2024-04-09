dd2746200dc69bba8952833bcc89c9fed514fb61
kishankalavadiya@gmail.com <kishankalavadiya@gmail.com>
2024-04-09 17:04:09 +0530

    pass

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -9,21 +9,27 @@ export default function Navbar() {
           className=" m-2 select-none rounded-lg border border-gray-900 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-gray-900 transition-all hover:opacity-75 focus:ring focus:ring-gray-300 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
           type="button"
         >
-          <Link href="/signup">Sign up</Link>
+          <Link href="/signup" passHref>
+            Sign up
+          </Link>
         </button>
 
         <button
           className=" m-2 select-none rounded-lg border border-gray-900 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-gray-900 transition-all hover:opacity-75 focus:ring focus:ring-gray-300 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
           type="button"
         >
-          <Link href="/login">Login</Link>
+          <Link href="/login" passHref>
+            Login
+          </Link>
         </button>
 
         <button
           className=" m-2 select-none rounded-lg border border-gray-900 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-gray-900 transition-all hover:opacity-75 focus:ring focus:ring-gray-300 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
           type="button"
         >
-          <Link href="/profile">Profile</Link>
+          <Link href="/profile" passHref>
+            Profile
+          </Link>
         </button>
       </div>
     </>
