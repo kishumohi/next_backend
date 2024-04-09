5ff31a79ee04c6926ec8a6562c25fc0f81d44c35
kishankalavadiya@gmail.com <kishankalavadiya@gmail.com>
2024-04-09 16:54:22 +0530

    complete 08

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,58 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import axios from "axios";
+import Link from "next/link";
+import { toast } from "react-hot-toast";
+import { useRouter } from "next/navigation";
 
-const page = () => {
-  return <div>Profile page</div>;
-};
+export default function ProfilePage() {
+  const router = useRouter();
+  const [data, setData] = useState("nothing");
 
-export default page;
+  const getUserDetails = async () => {
+    try {
+      const res = await axios.post("/api/users/me");
+      console.log(res.data.data._id);
+      setData(res.data.data._id);
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  };
+
+  const logout = async () => {
+    try {
+      await axios.get("/api/users/logout");
+      toast.success("logout Success");
+      router.push("/login");
+    } catch (error: any) {
+      console.log(error.message);
+      toast.error(error.message);
+    }
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>Profile page</h1>
+      <h2>
+        {data === "nothing" ? (
+          "Nothing to Show"
+        ) : (
+          <Link href={`/profile/${data}`}>{data}</Link>
+        )}
+        <hr />
+        <button
+          className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={logout}
+        >
+          Logout
+        </button>
+        <button
+          className="bg-green-500 mt-4 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+          onClick={getUserDetails}
+        >
+          Get User Details
+        </button>
+      </h2>
+    </div>
+  );
+}
---
+++ src/components/navbar.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <>
+      <div className="p-2">
+        <button
+          className=" m-2 select-none rounded-lg border border-gray-900 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-gray-900 transition-all hover:opacity-75 focus:ring focus:ring-gray-300 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+          type="button"
+        >
+          <Link href="/signup">Sign up</Link>
+        </button>
+
+        <button
+          className=" m-2 select-none rounded-lg border border-gray-900 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-gray-900 transition-all hover:opacity-75 focus:ring focus:ring-gray-300 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+          type="button"
+        >
+          <Link href="/login">Login</Link>
+        </button>
+
+        <button
+          className=" m-2 select-none rounded-lg border border-gray-900 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-gray-900 transition-all hover:opacity-75 focus:ring focus:ring-gray-300 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+          type="button"
+        >
+          <Link href="/profile">Profile</Link>
+        </button>
+      </div>
+    </>
+  );
+}
