b2560119329dabcafe29629ca6ae95c7638f4a92
kishankalavadiya@gmail.com <kishankalavadiya@gmail.com>
2024-04-09 17:01:02 +0530

    midd

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,12 +10,12 @@ export function middleware(request: NextRequest) {
 
   const token = request.cookies.get("token")?.value || "";
 
-  if (isPublicPath && token) {
-    return NextResponse.redirect(new URL("/", request.url));
-  }
-  // if (!isPublicPath && !token) {
-  //   return NextResponse.redirect(new URL("/login", request.url));
+  // if (isPublicPath && token) {
+  //   return NextResponse.redirect(new URL("/", request.url));
   // }
+  if (!isPublicPath && !token) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
 }
 
 // See "Matching Paths" below to learn more
